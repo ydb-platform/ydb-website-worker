1f1b76f6c16010c957d776ec5ce18604401dd8a7
Ivan Blinkov <ivan@ydb.tech>
2023-12-13 19:29:58 +0700

    Fix cli_win url in config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export default {
   install_sh_location: {
     server: new URL('https://binaries.ydb.tech/local_scripts/install.sh'),
     cli: new URL('https://storage.yandexcloud.net/yandexcloud-ydb/install.sh'),
-    cli_win: new URL('https://storage.ydb.tech/yandexcloud-ydb/install.ps1')
+    cli_win: new URL('https://storage.yandexcloud.net/yandexcloud-ydb/install.ps1')
   },
 
   production: true,
